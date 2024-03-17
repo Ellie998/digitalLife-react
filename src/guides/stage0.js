925ee63a51761838eb050a1a88bd6780de37cb9e
Ellie <dpfla9144@naver.com>
2024-03-17 19:41:18 +0900

    push origin

--- src/guides/stage0.js
+++ src/guides/stage0.js
@@ -1,38 +1,39 @@
 function Stage0(prop) {
+  function stageChange() {
+    // const sam
+  }
 
-    function stageChange(){
-        const sam
-    }
+  return (
+    <div className="sample_wrap 0">
+      <input
+        type="text"
+        id="id"
+        name="id"
+        placeholder="아이디"
+        title="아이디"
+        className="input_text"
+        maxLength="41"
+      />
+      <input
+        type="text"
+        id="pw"
+        name="pw"
+        placeholder="비밀번호"
+        title="비밀번호"
+        className="input_text"
+        maxLength="16"
+      />
 
-    return (
-        <div className="sample_wrap 0">
-            <input
-                type="text"
-                id="id"
-                name="id"
-                placeholder="아이디"
-                title="아이디"
-                className="input_text"
-                maxLength="41"
-            />
-            <input
-                type="text"
-                id="pw"
-                name="pw"
-                placeholder="비밀번호"
-                title="비밀번호"
-                className="input_text"
-                maxLength="16"
-            />
-
-            <div className="btn_login_wrap">
-                <button type="submit" className="btn_login" id="log.login">
-                    <span className="btn_text">로그인</span>
-                </button>
-                <div className="text_findID" onClick={stageChange}>아이디 찾기</div>
-            </div>
+      <div className="btn_login_wrap">
+        <button type="submit" className="btn_login" id="log.login">
+          <span className="btn_text">로그인</span>
+        </button>
+        <div className="text_findID" onClick={stageChange}>
+          아이디 찾기
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Stage0;
